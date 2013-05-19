2801fdd4b06c688a7f8e08304fea017d66bead57
Dylan Whichard <dylan@whichard.com>
2013-05-19 18:06:36 -0400

    Added helper functions for nodes Added some helper funtions for caching LenNodes now takes a bool parameter, and includes cached nodes if true

--- database.go
+++ database.go
@@ -28,14 +28,31 @@ updated DATETIME DEFAULT CURRENT_TIMESTAMP);`)
 	if err != nil {
 		return
 	}
+	_, err = db.Query(`CREATE TABLE IF NOT EXISTS nodes_cached (
+address BINARY(16) PRIMARY KEY,
+owner VARCHAR(255) NOT NULL,
+lat FLOAT NOT NULL,
+lon FLOAT NOT NULL,
+status INT NOT NULL,
+source VARCHAR(255) NOT NULL,
+retrieved DATETIME DEFAULT CURRENT_TIMESTAMP,
+expiration DATETIME);`)
+	if err != nil {
+		return
+	}
 	return
 }
 
 // LenNodes returns the number of nodes in the database. If there is
 // an error, it returns zero and logs the incident.
-func (db DB) LenNodes() (n int) {
+func (db DB) LenNodes(useCached bool) (n int) {
 	// Count the number of rows in the 'nodes' table.
-	row := db.QueryRow("SELECT COUNT(*) FROM nodes;")
+	var row *sql.Row
+	if useCached {
+		row = db.QueryRow("SELECT COUNT(*) FROM (SELECT address FROM nodes UNION SELECT address FROM nodes_cached);")
+	} else {
+		row = db.QueryRow("SELECT COUNT(*) FROM nodes;")
+	}
 	// Write that number to n, and return if there is no
 	// error. Otherwise, log it and return zero.
 	if err := row.Scan(&n); err != nil {
@@ -52,9 +69,70 @@ func (db DB) AddNode(node *Node) (err error) {
 (address, owner, lat, lon, status)
 VALUES(?, ?, ?, ?, ?)`)
 	if err != nil {
-		return err
+		stmt.Close()
+		return
 	}
 	stmt.Exec(node.Addr, node.OwnerName, node.Latitude, node.Longitude, node.Status)
 	stmt.Close()
-	return nil
+	return
+}
+
+func (db DB) UpdateNode(node *Node) (err error) {
+	// Updates an existing node in the database
+	stmt, err := db.Prepare(`UPDATE nodes SET 
+owner = ?, lat = ?, lon = ?, status = ?
+WHERE address = ?`)
+	if err != nil {
+		stmt.Close()
+		return
+	}
+	stmt.Exec(node.OwnerName, node.Latitude, node.Longitude, node.Status, node.Addr)
+	stmt.Close()
+	return
+}
+
+func (db DB) DeleteNode(node *Node) (err error) {
+	// Deletes the given node from the database
+	stmt, err := db.Prepare("DELETE FROM nodes WHERE address = ? LIMIT 1")
+	if err != nil {
+		stmt.Close()
+		return
+	}
+	stmt.Exec(node.Addr)
+	stmt.Close()
+	return
+}
+
+func (db DB) GetNode(addr string) (node *Node) {
+	// Retrieves the node with the given address from the database
+	stmt, err := db.Prepare(`SELECT address, owner, lat, lon, status
+FROM nodes
+WHERE address = ?
+UNION
+SELECT address, owner, lat, lon, status
+FROM nodes_cached
+WHERE address = ?
+LIMIT 1`)
+	if err != nil {
+		stmt.Close()
+		node = nil
+		return
+	}
+	row := stmt.QueryRow(addr, addr)
+	row.Scan(&node.Addr, &node.OwnerName, &node.Latitude, &node.Longitude, &node.Status)
+	stmt.Close()
+	return
+}
+
+func (db DB) CacheNode(node *Node, source string, expiry int) (err error) {
+	stmt, err := db.Prepare(`INSERT INTO nodes_cached
+(address, owner, lat, lon, status, source, expiration)
+VALUES(?, ?, ?, ?, ?, ?, ?)`)
+	if err != nil {
+		stmt.Close()
+		return
+	}
+	stmt.Exec(node.Addr, node.OwnerName, node.Latitude, node.Longitude, node.Status, source, expiry)
+	stmt.Close()
+	return
 }
