150b266b1e933794e12333b22df22752175d7904
Luke Evers <me@lukevers.com>
2013-06-21 14:18:08 -0400

    Getting the two nodes (and only two) works now!
    Ref #54

--- res/js/distance.js
+++ res/js/distance.js
@@ -1,3 +1,22 @@
-function init() {
-	
+var loc1;
+var loc2;
+
+function initDistance() {
+	map.on('popupopen', onDistanceClick);
+}
+
+function onDistanceClick(e) {
+	var loc = e.popup._source.getLatLng();
+	if (typeof loc1 == 'undefined') {
+		loc1 = loc;
+	} else if (loc == loc1) {
+		// TODO: create a real error message with
+		//       the nice popup thing that has yet
+		//       to be written yet.
+		// You already picked this node.
+		alert('You have already picked this node.');
+	} else if (typeof loc2 == 'undefined') {
+		loc2 = loc;
+		map.off('popupopen', onDistanceClick);
+	} 
 }
