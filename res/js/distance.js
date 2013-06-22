4cf55ce849b084fdd86bfa169cb7efbd800fa1d6
Luke Evers <me@lukevers.com>
2013-06-22 08:20:21 -0400

    Added a popup on the line hover

--- res/js/distance.js
+++ res/js/distance.js
@@ -5,10 +5,6 @@ function initDistance() {
 	map.on('popupopen', onDistanceClick);
 }
 
-function removeLine(e) {
-	map.removeLayer(this);
-}
-
 function onDistanceClick(e) {
 	var loc = e.popup._source.getLatLng();
 	if (typeof loc1 == 'undefined') {
@@ -22,21 +18,30 @@ function onDistanceClick(e) {
 	} else if (typeof loc2 == 'undefined') {
 		loc2 = loc;
 		map.off('popupopen', onDistanceClick);
-		drawLine([loc1, loc2]);
-		alertDistance(findDistance(loc1, loc2));
+		findDistance(loc1, loc2);
 		loc1 = undefined;
 		loc2 = undefined;
 	} 
 }
 
-function drawLine(points) {
+function drawLine(points, distance, center) {
 	var line = new L.Polyline(points, {
 		color: '#000',
 		weight: 5,
 		opacity: 0.5,
 		smoothFactor: 1
 	});
-	line.on('click', removeLine);
+	
+	var popup = L.popup();
+	popup.setLatLng(line.getBounds().getCenter());
+	popup.setContent(distance+'km');
+	
+	line.on('click', function() {
+		map.removeLayer(this);
+	});
+	line.on('mouseover', function() {
+		popup.addTo(map);
+	});
 	line.addTo(map);
 }
 
@@ -56,11 +61,7 @@ function findDistance(loc1, loc2) {
 			Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 	var d = R * c;
-	return d;
-}
-
-function alertDistance(distance) {
-	alert(distance+'km');
+	drawLine([loc1, loc2], d);
 }
 
 Number.prototype.toRad = function() {
