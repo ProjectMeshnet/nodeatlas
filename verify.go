734d8316470fd6394b4236c16710aa1bf87e24a8
Charles Lehner <celehner1@gmail.com>
2013-07-06 15:22:40 -0400

    Fix typo

--- verify.go
+++ verify.go
@@ -89,8 +89,8 @@ func SendVerificationEmail(id int64, n *Node) (err error) {
 	data["InstanceName"] = Conf.Name
 	data["Link"] = "http://" + Conf.Web.Hostname + Conf.Web.Prefix
 	data["VerificationID"] = id
-	data["FromEmail"] = Conf.SMTP.EmailAddress
-	data["ToEmail"] = n.OwnerEmail
+	data["FromAddress"] = Conf.SMTP.EmailAddress
+	data["ToAddress"] = n.OwnerEmail
 	const layout = "Mon, 2 Jan 2006 15:04:05 -0700"
 	date["Date"] = time.Now().Format(layout)
 
