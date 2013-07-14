cb21de2af2331e16599273201f83268ea00d0baf
Sasha Crofter <sasha@crofter.org>
2013-07-14 16:38:13 -0400

    Add import utilities
    Ref #62

---
+++ import.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+// Import reads a slice of JSON-encoded Nodes from the given io.Reader
+// and adds them to the database. Cache-related fields such as
+// RetrieveTime are discarded.
+func Import(r io.Reader) (err error) {
+	// Unmarshal the Nodes from the given io.Reader.
+	nodes := make([]*Node, 0)
+	err = json.NewDecoder(r).Decode(&nodes)
+	if err != nil {
+		return
+	}
+
+	// Insert them into the database as new. Timestamps will be the
+	// current time.
+	err = Db.AddNodes(nodes)
+	return
+}
+
+// ImportFile opens the given file and imports JSON-encoded Nodes from
+// it.
+func ImportFile(path string) (err error) {
+	// Open the file in readonly mode.
+	f, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	// Pass it along to Import.
+	return Import(f)
+}
