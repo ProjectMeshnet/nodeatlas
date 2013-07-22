fa48c3ab93182107ceed5b32e188e4f1e02d26ee
Luke Evers <me@lukevers.com>
2013-07-22 17:03:26 -0400

    Verify node if going to /verify/
    Ref #117

--- res/web/js/verify.js
+++ res/web/js/verify.js
@@ -1,21 +1,19 @@
-function verifyNode() {
+function verifyNode(key) {
 	// verifyNode submits POST /api/verify with the ID being the last
 	// element of the current URL, (e.g. /verify/012345).
-	var path = window.location.pathname.split( '/' );
-	var id = path[ path.length - 1 ];
 
 	// Once the ID has been retrieved, POST it to /api/verify.
 	$.ajax({
 		type: "GET",
 		url: "/api/verify",
-		data: { "id": id },
+		data: { "id": key },
 		success: function() {
-			$('#status').text("Successful").hide().fadeIn(1000);
+			//$('#status').text("Successful").hide().fadeIn(1000);
+			alert('success');
 		},
 		error: function(response) {
-			$('#status').text("Unsuccessful").hide().fadeIn(1000);
+			//$('#status').text("Unsuccessful").hide().fadeIn(1000);
+			alert('error');
 		}
 	});
 }
-
-verifyNode()
