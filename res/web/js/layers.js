f939ac9490ae5e5f38ca2f10eea6b47dff94e73d
Luke Evers <me@lukevers.com>
2013-08-03 18:46:19 -0400

    Added basic support for layers.
    Layers don't actually work yet, but they will.

---
+++ res/web/js/layers.js
@@ -0,0 +1,27 @@
+function activeNodes() {
+	
+}
+
+function potentialNodes() {
+	
+}
+
+function residentialNodes() {
+	
+}
+
+function vpsNodes() {
+	
+}
+
+function internetNodes() {
+	
+}
+
+function wirelessNodes() {
+	
+}
+
+function wiredNodes() {
+	
+}
