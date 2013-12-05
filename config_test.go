67675037c28e2dd3e8b81bcff8976b9a7f7248d6
Luke Evers <me@lukevers.com>
2013-12-05 07:37:26 -0500

    TestWriteConfig

--- config_test.go
+++ config_test.go
@@ -3,14 +3,28 @@ package main
 import (
 	"testing"
 	"fmt"
+	"os"
 )
 
 var err error
+var conf *Config
 
 func TestReadConfig(t *testing.T) {
 	fmt.Println("Testing Read Config")
-	_, err = ReadConfig("conf.json.example")
+	conf, err = ReadConfig("conf.json.example")
 	if err != nil {
 		t.Errorf("Could not read config: %s", err)
+	}
+}
+
+func TestWriteConfig(t *testing.T) {
+	fmt.Println("Testing Write Config")
+	err = WriteConfig(conf, "conf.write.test")
+	if err != nil {
+		t.Errorf("Could not write config: %s", err)
+	}
+	err = os.Remove("conf.write.test")
+	if err != nil {
+		t.Errorf("Could not delete test config: %s", err)
 	}
 }
