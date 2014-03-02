e1175e5652055a5a3533351f44694889c45d0a95
Luke Evers <me@lukevers.com>
2014-03-02 13:27:08 -0500

    Added the HTML for where the curl/wget data will go.
    Ref #143

--- res/web/js/form.js
+++ res/web/js/form.js
@@ -7,23 +7,23 @@ function createMessageForm(name, ipv6) {
     html += '<br/><input type="text" placeholder="Subject" id="subject" class="form-control"  maxlength="255">';
     html += '<br/><textarea placeholder="Body" id="message" class="form-control" maxlength="999"></textarea>';
     html += '<br/><div class="text-center disabled">Basic markdown is supported</div>';
-    html += '<br/><div class="row"><div class="col col-lg-6 text-center">';
+    html += '<br/><div class="row"><div class="col col-xs-6 text-center">';
     html += '<input type="reset" id="cancelmessage" class="btn btn-default btn-small" value="Cancel Message"></div>';
-    html += '<div class="col col-lg-6 text-center">';
+    html += '<div class="col col-xs-6 text-center">';
     html += '<input type="submit" id="nextpagesubmit" class="btn btn-small btn-primary" value="Next Page" onclick="next(2); return false;"></div></div>';
     html += '</div><div id="two">';
     html += '<img id="captcha_img">';
     html += '<br/><input type="text" placeholder="Captcha" class="form-control" id="captcha" maxlength="255">';
     html += '<br/><div class="text-center"><button class="btn btn-warning btn-mini" href="#" onclick="loadCAPTCHA($(\'#captcha_img\')); return false;">Load New Captcha</button></div>';
-    html += '<br/><div class="row"><div class="col col-lg-6 text-center">';
+    html += '<br/><div class="row"><div class="col col-xs-6 text-center">';
     html += '<input type="reset" onclick="next(1); return false;" class="btn btn-default btn-small" value="Previous Page"></div>';
-    html += '<div class="col col-lg-6 text-center">';
+    html += '<div class="col col-xs-6 text-center">';
     html += '<input type="submit" id="sendmessage" class="btn btn-small btn-primary" value="Send Message"></div></div>';
     html += '</div></div>';
     return html;
 }
 
-function getForm(lat, lng) { 
+function getForm(lat, lng) {
     var form =  '<div id="inputform">';
     form += '<div class="tabby">';
     form += '<div class="tab" id="one">';
@@ -37,9 +37,9 @@ function getForm(lat, lng) {
     form += '<input type="text" class="input-medium form-control" id="address" name="address" placeholder="Required" maxlength="39"/>';
     form += '<label><strong>Details</strong></label>';
     form += '<input type="text" class="input-medium form-control" placeholder="Home, Work, ..." id="details" maxlength="255"/><br/>';
-    form += '<div class="row"><div class="col col-lg-6 text-center">';
+    form += '<div class="row"><div class="col col-xs-6 text-center">';
     form += '<button class="btn btn-default btn-small" href="#" onclick="cancelRegistration(); return false;">Cancel</button></div>';
-    form += '<div class="col col-lg-6 text-center">';
+    form += '<div class="col col-xs-6 text-center">';
     form += '<button class="btn btn-small btn-primary" href="#" onclick="next(2); return false;">Next</button></div></div>';
     form += '</div>';
     form += '<div class="tab" id="two">';
@@ -52,9 +52,9 @@ function getForm(lat, lng) {
     form += '<input type="checkbox" id="residential"> ';
     form += 'Residential node';
     form += '</label><br/><br/>';
-    form += '<div class="row"><div class="col col-lg-6 text-center">';
+    form += '<div class="row"><div class="col col-xs-6 text-center">';
     form += '<button class="btn btn-small btn-primary" href="#" onclick="next(1); return false;">Back</button></div>';
-    form += '<div class="col col-lg-6 text-center">';
+    form += '<div class="col col-xs-6 text-center">';
     form += '<button class="btn btn-small btn-primary" href="#" onclick="next(3); return false;">Next</button></div></div>';
     form += '</div>';
     form += '<div class="tab" id="three">';
@@ -72,9 +72,9 @@ function getForm(lat, lng) {
     form += 'Wired (eth) access';
     form += '</label><br/><br/>';
     form += '<div class="row">';
-    form += '<div class="col col-lg-6 text-center">';
+    form += '<div class="col col-xs-6 text-center">';
     form += '<button class="btn btn-small btn-primary" href="#" onclick="next(2); return false;">Back</button></div>';
-    form += '<div class="col col-lg-6 text-center">';
+    form += '<div class="col col-xs-6 text-center">';
     form += '<button class="btn btn-small btn-primary" href="#" onclick="next(4); return false;">Next</button></div></div>';
     form += '</div>';
     form += '<div class="tab" id="four">';
@@ -84,9 +84,20 @@ function getForm(lat, lng) {
     form += '<textarea class="contact form-control" id="contact" placeholder="XMPP username, Reddit username, ..." maxlength="255"></textarea><br/>';
     form += '<input style="display: none;" type="text" id="latitude" name="latitude" value="'+lat+'"/>';
     form += '<input style="display: none;" type="text" id="longitude" name="longitude" value="'+lng+'"/>';
-    form += '<div class="row"><div class="col col-lg-6 text-center">';
+    form += '<div class="row"><div class="col col-xs-6 text-center">';
     form += '<button class="btn btn-small btn-primary" href="#" onclick="next(3); return false;">Back</button></div>';
-    form += '<div class="col col-lg-6 text-center">';
+    form += '<div class="col col-xs-6 text-center">';
+    form += '<button class="btn btn-small btn-primary" href="#" onclick="next(5); return false;">Next</button></div></div></div>';
+    form += '<div class="tab" id="five"><div class="row">';
+    form += '<div class="col col-xs-12">';
+    form += '<label>Submit via curl</label>';
+    form += '<input type="text" class="form-control" placeholder="Curl Data" id="curl">';
+    form += '<label>Submit via wget</label>';
+    form += '<br/><input type="text" class="form-control" placeholder="Wget Data" id="wget">';
+    form += '<br/></div></div>';
+    form += '<div class="row"><div class="col col-xs-6 text-center">';
+    form += '<button class="btn btn-small btn-primary" href="#" onclick="next(4); return false;">Back</button></div>';
+    form += '<div class="col col-xs-6 text-center">';
     form += '<button class="btn btn-small btn-info" onclick="insertUser(); return false;" id="submitatlas">Submit</button></div></div>';
     form += '</div>';
     form += '</div>';
@@ -95,25 +106,26 @@ function getForm(lat, lng) {
 }
 
 function next(which) {
-    if (which == 1) {
-	$('#one').css('display', 'block');
-	$('#two').css('display', 'none');
-	$('#three').css('display', 'none');
-	$('#four').css('display', 'none');
-    } else if (which == 2) {
-	$('#one').css('display', 'none');
-	$('#two').css('display', 'block');
-	$('#three').css('display', 'none');
-	$('#four').css('display', 'none');
-    } else if (which == 3) {
-	$('#one').css('display', 'none');
-	$('#two').css('display', 'none');
-	$('#three').css('display', 'block');
-	$('#four').css('display', 'none');
-    } else if (which == 4) {
-	$('#one').css('display', 'none');
-	$('#two').css('display', 'none');
-	$('#three').css('display', 'none');
-	$('#four').css('display', 'block');
-    }
+  switch (which) {
+    case 1:
+      $('#one').css('display', 'block');
+      $('#two, #three, #four, #five').css('display', 'none');
+      break;
+    case 2:
+      $('#two').css('display', 'block');
+      $('#one, #three, #four, #five').css('display', 'none');
+      break;
+    case 3:
+      $('#three').css('display', 'block');
+      $('#one, #two, #four, #five').css('display', 'none');
+      break;
+    case 4:
+      $('#four').css('display', 'block');
+      $('#one, #two, #three, #five').css('display', 'none');
+      break;
+    case 5:
+      $('#five').css('display', 'block');
+      $('#one, #two, #three, #four').css('display', 'none');
+      break;
+  }
 }
